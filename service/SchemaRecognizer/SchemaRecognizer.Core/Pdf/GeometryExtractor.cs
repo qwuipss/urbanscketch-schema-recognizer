9191b6c8137b76b1b79350b0e598b123f146829b
ivan <qwuipss@icloud.com>
2025-11-30 01:40:21 +0500

    geometry extractor

--- service/SchemaRecognizer/SchemaRecognizer.Core/Pdf/GeometryExtractor.cs
+++ service/SchemaRecognizer/SchemaRecognizer.Core/Pdf/GeometryExtractor.cs
@@ -1,62 +1,27 @@
-using iText.Kernel.Pdf.Canvas.Parser;
-using iText.Kernel.Pdf.Canvas.Parser.Data;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Core;
 
 namespace SchemaRecognizer.Core.Pdf;
 
-public class VectorShape
+public class GeometryExtractor : IGeometryExtractor
 {
-    public string Operation { get; set; } // Stroke, Fill, FillStroke
-
-    public string ColorStroke { get; set; }
-
-    public string ColorFill { get; set; }
-
-    public float LineWidth { get; set; }
-
-    public List<float[]> Points { get; set; } = new();
-}
-
-public class VectorShapeCollector : IEventListener
-{
-    public List<VectorShape> Shapes { get; } = new();
-
-    public void EventOccurred(IEventData data, EventType type)
+    public List<PdfSubpath> Extract(FileInfo fileInfo)
     {
-        if (type != EventType.RENDER_PATH) return;
+        using var doc = PdfDocument.Open(fileInfo.FullName);
+        var page = doc.GetPages().Single();
+        var l = new List<PdfSubpath>();
 
-        var info = (PathRenderInfo)data;
-        var path = info.GetPath();
-        var op = info.GetOperation();
-
-        var shape = new VectorShape
+        foreach (var path in page.Paths)
         {
-            Operation = op.ToString(),
-            LineWidth = info.GetGraphicsState().GetLineWidth()
-        };
-
-        // Stroke color
-        var strokeColor = info.GetGraphicsState().GetStrokeColor();
-        if (strokeColor != null)
-            shape.ColorStroke = strokeColor.ToString()!;
-
-        // Fill color
-        var fillColor = info.GetGraphicsState().GetFillColor();
-        if (fillColor != null)
-            shape.ColorFill = fillColor.ToString()!;
-
-        // // Points
-        // foreach (var segment in path.GetPathSegments())
-        // {
-        //     foreach (var p in segment.GetBasePoints())
-        //     {
-        //         shape.Points.Add(new float[] { p.Get(0), p.Get(1) });
-        //     }
-        // }
-
-        Shapes.Add(shape);
+            foreach (var subPath in path)
+            {
+                if (subPath.IsClosed())
+                {
+                    l.Add(subPath);
+                }
+            }
+        }
+        
+        return l;
     }
-
-    public ICollection<EventType> GetSupportedEvents() => null!;
 }
