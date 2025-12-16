994ddb5e85c937209d663e7545120e6d9b7ce55e
ivan <qwuipss@icloud.com>
2025-12-17 01:05:54 +0500

    rasterization debug

--- service/SchemaRecognizer/SchemaRecognizer.Debug/Program.cs
+++ service/SchemaRecognizer/SchemaRecognizer.Debug/Program.cs
@@ -1,12 +1,12 @@
 ﻿using IronSoftware.Drawing;
 
-var pdf = PdfDocument.FromFile("Example.pdf");
+var pdf = PdfDocument.FromFile(Path.Join(Environment.CurrentDirectory, "../../../../", "pdf/vector/v1.pdf"));
 
 // Extract all pages to a folder as image files
-pdf.RasterizeToImageFiles(Path.Join(Environment.CurrentDirectory, "../../../../", "pdf/dev/v2-geom.pdf"));
+pdf.RasterizeToImageFiles(Path.Join(Environment.CurrentDirectory, "../../../../", "pdf/dev/jpg.jpg"));
 
 // Dimensions and page ranges may be specified
-pdf.RasterizeToImageFiles(@"C:\image\folder\example_pdf_image_*.jpg", 100, 80);
+// pdf.RasterizeToImageFiles(@"C:\image\folder\example_pdf_image_*.jpg", 100, 80);
 
 // Extract all pages as AnyBitmap objects
 AnyBitmap[] pdfBitmaps = pdf.ToBitmap();
